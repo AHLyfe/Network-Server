d0746a17df15d20baea2b4add9362f66ca297512
AllTheHotkeys <ally@hvitt.co.uk>
2016-06-23 20:59:24 +0100

    Server v0.1.2

--- src/gui/Panel.java
+++ src/gui/Panel.java
@@ -5,6 +5,8 @@ import java.awt.Dimension;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 
@@ -14,12 +16,28 @@ public class Panel extends JPanel{
 		setLayout(new BorderLayout());
 		
 		add(getPlayerList(), "North");
+		add(getLog(), "Center");
 	}
 	
 	public JScrollPane getPlayerList(){
 		PlayerList playerList = new PlayerList();
 		JScrollPane scroll = new JScrollPane(playerList);
-		scroll.setBorder(new TitledBorder(new EtchedBorder(), "Player List"));
+		scroll.setBorder(new TitledBorder(new EtchedBorder(), "Connected Players"));
 		return scroll;
+	}
+	
+	public JPanel getLog(){
+		JPanel panel = new JPanel(new BorderLayout());
+		JTextArea text = new JTextArea();
+		JScrollPane pane = new JScrollPane(text);
+		text.setEditable(false);
+		
+		JTextField input = new JTextField();
+		
+		panel.add(pane, "Center");
+		panel.add(input, "South");
+		panel.setBorder(new TitledBorder(new EtchedBorder(), "Log Output"));
+		
+		return panel;
 	}
 }
