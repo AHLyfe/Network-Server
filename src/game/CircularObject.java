c5915e82e5b0a4edf0acf86a7172d5d27fb8feb1
AllTheHotkeys <ally@hvitt.co.uk>
2016-06-27 22:50:40 +0100

    Added new GameObjects

---
+++ src/game/CircularObject.java
@@ -0,0 +1,11 @@
+package game;
+
+public class CircularObject extends GameObject {
+
+	@Override
+	void update(double dt) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/game/RectangularObject.java
@@ -0,0 +1,11 @@
+package game;
+
+public class RectangularObject extends GameObject {
+
+	@Override
+	void update(double dt) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
